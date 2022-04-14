0730b00c3b5cb9a93906e57576989d3a502f290c
ekayudinata <ekayudinatha@gmail.com>
2022-04-14 15:56:45 +0800

    add pateint folder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PatientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/dashboard',function (){
+    return view('dashboard.layouts.main', [
+        'title' => "dashboard"
+    ]); 
+}); 
+
+Route::resource('/dashboard/pasien', PatientController::class); 
+
