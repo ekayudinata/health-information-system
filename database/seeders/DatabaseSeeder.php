35ca3afc14feba5a31ea3163899a0961d1c1c589
adipagastya <adipa.pt@gmail.com>
2022-04-15 16:13:53 +0800

    config migration, seeder user and patient

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+use App\Models\Patient;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +17,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Adipa Agastya',
+            'email' => 'adipa@omsamedic.com',
+            'password' => bcrypt('12345'),
+            'user_role_id' => 1,
+            'employee_id' => 1
+        ]);
+
+        Patient::create([
+            'medic_record' => 'U# 309854',
+            'name' => 'NI KETUT SARMAWI',
+            'gender' => 'P',
+            'birth_date' => '1970-02-23',
+            'address' => 'JL. GADUNG GG XI/3 DPS',
+            'phone' => '082147958349',
+            'nationality' => 'WNI',
+            'id_card_number' => '3306124403910302',
+            'bpjs_number' => '0001308270036',
+            'bpjs_medic' => 'FASKES JIMBARAN'
+        ]);
     }
 }
