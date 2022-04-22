9048859260ebf6ea2cf7a33331b3e69566abc464
adipagastya <adipa.pt@gmail.com>
2022-04-22 15:19:46 +0800

    config clinic migrate

--- database/migrations/2022_04_22_064714_create_clinics_table.php
+++ database/migrations/2022_04_22_064714_create_clinics_table.php
@@ -15,6 +15,9 @@ class CreateClinicsTable extends Migration
     {
         Schema::create('clinics', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
             $table->timestamps();
         });
     }
